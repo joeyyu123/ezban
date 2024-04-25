52bfc7741236aa42db37a3e288a830f61d233d38
erinchc <erinchen25@gmail.com>
2024-04-25 12:54:32 +0800

    新增前台template及application-erin.properties

---
+++ src/main/java/ezban/controller/FrontstageController.java
@@ -0,0 +1,21 @@
+package ezban.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/frontstage")
+public class FrontstageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "/frontstage/index2";
+    }
+
+    @GetMapping("/template")
+    public String template() {
+        return "/frontstage/template2";
+    }
+
+}
