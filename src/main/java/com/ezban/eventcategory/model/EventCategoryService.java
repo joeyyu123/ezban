15e09ada7889183cfefaf5e5387d8b6a6119d8f1
JoeyYu <joeyyu159357789@gmail.com>
2024-05-06 15:18:41 +0800

    新增EventCategoryService、EventCategory、EventCategoryRepository檔案
    - 在EventCategoryService中複寫add、update、findById、findAll方法
    
    - 在EventCategory中，將"id"更名為"eventCategoryNo"，並修正相應的getter和setter方法。
    
    - 新增EventCategoryRepository，提供對資料庫的操作。

---
+++ src/main/java/com/ezban/eventcategory/model/EventCategoryService.java
@@ -0,0 +1,35 @@
+package com.ezban.eventcategory.model;
+
+import com.ezban.event.model.ServiceDemo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class EventCategoryService implements ServiceDemo<EventCategory> {
+
+    @Autowired
+    EventCategoryRepository repository;
+
+    @Override
+    public EventCategory add(EventCategory vo) {
+        return repository.saveAndFlush(vo);
+    }
+
+    @Override
+    public EventCategory update(EventCategory vo) {
+        return repository.saveAndFlush(vo);
+    }
+
+    @Override
+    public EventCategory findById(Integer id) {
+        return repository.getReferenceById(id);
+    }
+
+    @Override
+    public List<EventCategory> findAll() {
+        return repository.findAll();
+    }
+}
