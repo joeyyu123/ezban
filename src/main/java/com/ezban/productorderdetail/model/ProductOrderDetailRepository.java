935b5d6d8231ac2aa132ffe1aa14a6ab15af079b
Jim <xiou7777@gmail.com>
2024-05-10 01:01:54 +0800

    ProductOrder更正為ProductOrderDetail

--- src/main/java/com/ezban/productorderdetail/model/ProductOrderDetailRepository.java
+++ src/main/java/com/ezban/productorderdetail/model/ProductOrderDetailRepository.java
@@ -1,6 +1,5 @@
 package com.ezban.productorderdetail.model;
 
-import com.ezban.productorder.model.ProductOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,5 +8,5 @@ import java.util.List;
 public interface ProductOrderDetailRepository extends JpaRepository<ProductOrderDetail,Integer> {
 
     @Query(value = "select * from product_order_detail pd where pd.product_order_no = ?1", nativeQuery = true)
-    List<ProductOrder> findByProductOrder(Integer productOrderNo);
+    List<ProductOrderDetail> findByProductOrder(Integer productOrderNo);
 }
