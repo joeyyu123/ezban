a9b1f2b94a5b1c4220ca7d55b7efddcd84134244
JoeyYu <joeyyu159357789@gmail.com>
2024-05-09 22:32:50 +0800

    後台活動報名表設定功能完成

--- src/main/java/com/ezban/registrationform/model/RegistrationFormService.java
+++ src/main/java/com/ezban/registrationform/model/RegistrationFormService.java
@@ -1,8 +1,14 @@
 package com.ezban.registrationform.model;
 
+import com.ezban.fieldExample.model.FieldExample;
+import com.ezban.ticketorder.model.dto.TicketOrderRegistrationForm;
+import com.ezban.ticketorderdetail.model.TicketOrderDetail;
+import com.ezban.ticketorderdetail.model.TicketOrderDetailService;
+import com.ezban.ticketregistration.dto.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -10,11 +16,46 @@ public class RegistrationFormService {
     @Autowired
     RegistrationFormRepository registrationFormRepository;
 
+    @Autowired
+    TicketOrderDetailService ticketOrderDetailService;
+
     public RegistrationForm findByEventNo(String eventNo) {
         return registrationFormRepository.getByEventNo(eventNo);
     }
 
     public void save(RegistrationForm registrationForm) {
         registrationFormRepository.save(registrationForm);
+    }
+
+    public List<TicketOrderRegistrationForm> getTicketOrderRegistrationForms(Integer eventNo, Integer orderNo) {
+        RegistrationForm registrationForm = this.findByEventNo(String.valueOf(eventNo));
+
+        // 將活動報名表轉換成會員填寫的報名表物件
+        List<Question> ticketOrderRegistrationFormQuestions = new ArrayList<>();
+        List<FieldExample> questions = registrationForm.getQuestions();
+        questions.forEach(question -> {
+            Question ticketOrderRegistrationFormQuestion = new Question();
+            ticketOrderRegistrationFormQuestion.setComponent(question.getComponent());
+            ticketOrderRegistrationFormQuestion.setLabel(question.getLabel());
+            ticketOrderRegistrationFormQuestion.setDescription(question.getDescription());
+            ticketOrderRegistrationFormQuestion.setOptions(question.getOptions());
+            ticketOrderRegistrationFormQuestion.setRequired(question.isRequired());
+            ticketOrderRegistrationFormQuestions.add(ticketOrderRegistrationFormQuestion);
+        });
+
+
+        List<TicketOrderRegistrationForm> ticketOrderRegistrationForms = new ArrayList<>();
+        List<TicketOrderDetail> ticketOrderDetails = ticketOrderDetailService.findByOrderNo(orderNo);
+        ticketOrderDetails.forEach(ticketOrderDetail -> {
+            TicketOrderRegistrationForm ticketOrderRegistrationForm = new TicketOrderRegistrationForm();
+            ticketOrderRegistrationForm.setTicketTypeNo(String.valueOf(ticketOrderDetail.getTicketType().getTicketTypeNo()));
+            ticketOrderRegistrationForm.setTicketTypeName(ticketOrderDetail.getTicketType().getTicketTypeName());
+            ticketOrderRegistrationForm.setTicketTypeQty(String.valueOf(ticketOrderDetail.getTicketTypeQty()));
+            ticketOrderRegistrationForm.setIncludedTickets(String.valueOf(ticketOrderDetail.getIncludedTicketQty()));
+            ticketOrderRegistrationForm.setQuestions(ticketOrderRegistrationFormQuestions);
+            ticketOrderRegistrationForms.add(ticketOrderRegistrationForm);
+        });
+
+        return ticketOrderRegistrationForms;
     }
 }
