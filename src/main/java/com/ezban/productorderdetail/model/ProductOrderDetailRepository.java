de50d805a685d41446b60af5ca881ab7d29eebf1
Jim <xiou7777@gmail.com>
2024-05-09 15:04:17 +0800

    ProductOrderDetailRepository增加findByProductOrder的Query

--- src/main/java/com/ezban/productorderdetail/model/ProductOrderDetailRepository.java
+++ src/main/java/com/ezban/productorderdetail/model/ProductOrderDetailRepository.java
@@ -1,10 +1,13 @@
 package com.ezban.productorderdetail.model;
 
+import com.ezban.productorder.model.ProductOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface ProductOrderDetailRepository extends JpaRepository<ProductOrderDetail,Integer> {
 
+    @Query(value = "select * from product_order_detail pd where pd.product_order_no = ?1", nativeQuery = true)
+    List<ProductOrder> findByProductOrder(Integer productOrderNo);
 }
