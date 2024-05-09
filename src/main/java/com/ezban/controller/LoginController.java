aed085cd54114abef6e777f6150b5cacb046c5e3
maxkuo625 <55643879+maxkuo625@users.noreply.github.com>
2024-05-09 15:48:24 +0800

    登入前後端完成(不包含第三方登入)

--- src/main/java/com/ezban/controller/LoginController.java
+++ src/main/java/com/ezban/controller/LoginController.java
@@ -1,27 +1,42 @@
 package com.ezban.controller;
 
-import com.ezban.member.model.MemebrAuthenticate;
+import java.util.Map;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import com.ezban.member.model.MemebrAuthenticate;
+
+@Controller
 public class LoginController {
 
     @Autowired
     private MemebrAuthenticate memberAuthenticate;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest loginRequest) {
         String memberMail = loginRequest.getMemberMail();
         String memberPwd = loginRequest.getMemberPwd();
 
-        ResponseEntity<String> response = memberAuthenticate.checkAccount(memberMail, memberPwd);
+        ResponseEntity<String> responseEntity = memberAuthenticate.checkAccount(memberMail, memberPwd);
 
-        return response;
+        Map<String, Object> responseObject = new HashMap<>();
+        responseObject.put("code", responseEntity.getStatusCodeValue());
+        responseObject.put("message", responseEntity.getBody());
+
+        return ResponseEntity.ok(responseObject);
+    }
+    
+    @GetMapping("/login")
+    public String getLogin() {
+        return "frontstage/login";
     }
 
     static class LoginRequest {
@@ -45,4 +60,3 @@ public class LoginController {
         }
     }
 }
-
