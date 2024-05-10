83761c391954768838e5abcf615eb743c1e887d5
JoeyYu <joeyyu159357789@gmail.com>
2024-05-10 16:36:56 +0800

    完成產生訂單以及訂單明細功能

--- src/main/java/com/ezban/ticketorder/model/TicketOrderService.java
+++ src/main/java/com/ezban/ticketorder/model/TicketOrderService.java
@@ -1,17 +1,30 @@
 package com.ezban.ticketorder.model;
 
 import com.ezban.event.model.ServiceDemo;
+import com.ezban.member.model.Member;
+import com.ezban.ticketorderdetail.model.TicketOrderDetail;
+import com.ezban.ticketorderdetail.model.TicketOrderDetailService;
+import com.ezban.ticketregistration.TicketRegistration;
 import com.ezban.tickettype.model.TicketType;
 import com.ezban.tickettype.model.TicketTypeRepository;
 import com.ezban.tickettype.model.TicketTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class TicketOrderService implements ServiceDemo<TicketOrder> {
+
+    @Autowired
+    private TicketTypeService ticketTypeService;
+
+    @Autowired
+    private TicketOrderDetailService ticketOrderDetailService;
 
     @Autowired
     private TicketOrderRepository ticketOrderRepository;
@@ -20,11 +33,49 @@ public class TicketOrderService implements ServiceDemo<TicketOrder> {
     private TicketTypeRepository ticketTypeRepository;
 
     @Override
+    @Transactional
     public TicketOrder add(TicketOrder vo) {
         return ticketOrderRepository.save(vo);
     }
 
+    @Transactional
+    public List<TicketOrderDetail> createOrder(Member member, List<Map<String, Integer>> orderDetailList) throws InsufficientTicketQuantityException {
+        TicketOrder ticketOrder = new TicketOrder();
+        ticketOrder.setMember(member);
+        ticketOrder.setTicketOrderTime(new Timestamp(System.currentTimeMillis()));
+        ticketOrder.setTicketOrderAmount(this.calculateTotalAmount(orderDetailList));
+        ticketOrder.setTicketOrderStatus((byte) 0);
+        ticketOrder.setTicketOrderPaymentStatus((byte) 0);
+        ticketOrder.setTicketCheckoutAmount(0);
+
+        ticketOrder = this.add(ticketOrder);
+
+        List<TicketOrderDetail> ticketOrderDetails = new ArrayList<TicketOrderDetail>();
+        for (Map<String, Integer> orderDetail : orderDetailList) {
+            TicketType ticketType = ticketTypeService.findById(orderDetail.get("ticketTypeNo"));
+
+            if (ticketType.getRemainingTicketQty() < orderDetail.get("ticketTypeQty")){
+                throw new InsufficientTicketQuantityException("票券已售完，請重新選擇。");
+            }
+            ticketType.setTicketTypeQty(ticketType.getRemainingTicketQty() - orderDetail.get("ticketTypeQty"));
+
+            TicketOrderDetail ticketOrderDetail = new TicketOrderDetail();
+            ticketOrderDetail.setTicketType(ticketType);
+            ticketOrderDetail.setTicketOrder(ticketOrder);
+            ticketOrderDetail.setTicketTypeQty(orderDetail.get("ticketTypeQty"));
+            ticketOrderDetail.setTicketTypePrice(ticketType.getTicketTypePrice());
+            ticketOrderDetail.setIncludedTicketQty(ticketType.getIncludedTickets());
+
+            ticketOrderDetails.add(ticketOrderDetail);
+        }
+
+        ticketOrderDetails = ticketOrderDetailService.saveAll(ticketOrderDetails);
+
+        return ticketOrderDetails;
+    }
+
     @Override
+    @Transactional
     public TicketOrder update(TicketOrder vo) {
         return ticketOrderRepository.save(vo);
     }
@@ -54,4 +105,6 @@ public class TicketOrderService implements ServiceDemo<TicketOrder> {
         }
         return totalAmount;
     }
+
+
 }
--- src/main/java/com/ezban/ticketorderdetail/model/TicketOrderDetailRepository.java
+++ src/main/java/com/ezban/ticketorderdetail/model/TicketOrderDetailRepository.java
@@ -1,7 +1,9 @@
 package com.ezban.ticketorderdetail.model;
 
-import com.ezban.ticketorder.model.TicketOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketOrderDetailRepository extends JpaRepository<TicketOrderDetail, Integer> {
+    List<TicketOrderDetail> findByTicketOrderTicketOrderNo(Integer ticketOrderNo);
 }
