b7426f521c2ff043ff36b0cd34845a3fb61bc1c1
Jim <xiou7777@gmail.com>
2024-05-11 14:03:07 +0800

    新增findByMember的條件查詢

--- src/main/java/com/ezban/saveproduct/model/SaveProductRepository.java
+++ src/main/java/com/ezban/saveproduct/model/SaveProductRepository.java
@@ -1,9 +1,16 @@
 package com.ezban.saveproduct.model;
 
+import com.ezban.productorder.model.ProductOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SaveProductRepository extends JpaRepository<SaveProduct,Integer> {
+
+    @Query(value = "select * from save_product sp where sp.member_no = ?1", nativeQuery = true)
+    List<SaveProduct> findByMember(Integer memberNo);
 
 }
