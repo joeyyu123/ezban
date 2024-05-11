eb0229771ca392c00120b44961cac2bcc1fe6ff0
Jim <xiou7777@gmail.com>
2024-05-11 14:18:58 +0800

    新增findByMember的條件查詢

--- src/main/java/com/ezban/saveproduct/model/SaveProductRepository.java
+++ src/main/java/com/ezban/saveproduct/model/SaveProductRepository.java
@@ -1,6 +1,5 @@
 package com.ezban.saveproduct.model;
 
-import com.ezban.productorder.model.ProductOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
