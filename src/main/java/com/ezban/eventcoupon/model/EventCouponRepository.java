caa6f6604807ba4fdab6c47ae13ba7ba9a796d60
Fred <fred890312@gmail.com>
2024-05-12 22:02:21 +0800

    更新EventCouponRepository

--- src/main/java/com/ezban/eventcoupon/model/EventCouponRepository.java
+++ src/main/java/com/ezban/eventcoupon/model/EventCouponRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EventCouponRepository extends JpaRepository<EventCoupon, Integer> {
     @Transactional
     @Modifying
     @Query(value = "delete from event_coupon where event_coupon_no =?1", nativeQuery = true)
     void deleteByEventCouponNo(int eventCouponNo);
+    Optional<EventCoupon> findByCouponCode(String couponCode);
 
     List<EventCoupon> findByHostHostNo(Integer hostNo);
 }
