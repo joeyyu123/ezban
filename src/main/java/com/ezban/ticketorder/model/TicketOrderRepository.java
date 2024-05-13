a51672b609ec889b7793264da9041ba52f861bd1
JoeyYu <joeyyu159357789@gmail.com>
2024-05-13 19:50:25 +0800

    新增檢查訂單是否在時間內付款的排程器

--- src/main/java/com/ezban/ticketorder/model/TicketOrderRepository.java
+++ src/main/java/com/ezban/ticketorder/model/TicketOrderRepository.java
@@ -2,5 +2,10 @@ package com.ezban.ticketorder.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface TicketOrderRepository extends JpaRepository<TicketOrder, Integer> {
+
+    List<TicketOrder> findByticketOrderStatusAndTicketOrderTimeBefore(TicketOrderStatus status, Timestamp time);
 }
