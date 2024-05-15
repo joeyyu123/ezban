1d453b904793710cad3cc60d2c30a6047b78e211
Jim <xiou7777@gmail.com>
2024-05-15 20:55:51 +0800

    1.新增-ProductOrderDetailService、商品明細的前後台controller、商品明細相關html(共3個)。 2.修改-ProductOrderDetail的Entity(原本用@Pattern改成@Min & @Max)。

---
+++ src/main/java/com/ezban/productorderdetail/controller/BackstageProductOrderDetailController.java
@@ -0,0 +1,38 @@
+package com.ezban.productorderdetail.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import com.ezban.productorderdetail.model.ProductOrderDetail;
+import com.ezban.productorderdetail.model.ProductOrderDetailService;
+
+@Controller
+@RequestMapping("/backstage/productorderdetail")
+public class BackstageProductOrderDetailController {
+
+    @Autowired
+    ProductOrderDetailService productOrderDetailSVC;
+
+    // 員工:依據訂單編號顯示明細
+    @PostMapping("/adminOrderDetail/{productOrderNo}")
+    public String adminOrderDetail(@PathVariable("productOrderNo") Integer productOrderNo, Model model) {
+        List<ProductOrderDetail> productOrderDetails = productOrderDetailSVC.findByProductOrder(productOrderNo);
+        model.addAttribute("productOrderDetailListData", productOrderDetails);
+        return "backstage/productorderdetail/listAllProductOrderDetail";
+    }
+
+
+    // 廠商:依據訂單編號顯示明細
+    @PostMapping("/hostOrderDetail/{productOrderNo}")
+    public String hostOrderDetail(@PathVariable("productOrderNo") Integer productOrderNo, Model model) {
+        List<ProductOrderDetail> productOrderDetails = productOrderDetailSVC.findByProductOrder(productOrderNo);
+        model.addAttribute("productOrderDetailListData", productOrderDetails);
+        return "backstage/productorderdetail/listAllProductOrderDetailByHost";
+    }
+
+}
+
+
