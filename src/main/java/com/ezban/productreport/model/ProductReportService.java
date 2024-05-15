b45feb7b225aba9e7b3cdcf546403b0132e09163
Jim <xiou7777@gmail.com>
2024-05-15 20:47:37 +0800

    1.新增-AddProductReportDTO、ProductReportService、UpdateProductReportDTO、前後台controller、檢舉商品相關html(共1個)。 2.修改-ProductReport的Entity。

---
+++ src/main/java/com/ezban/productreport/model/ProductReportService.java
@@ -0,0 +1,67 @@
+package com.ezban.productreport.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.ezban.admin.model.Admin;
+import com.ezban.admin.model.AdminRepository;
+import com.ezban.member.model.Member;
+import com.ezban.member.model.MemberRepository;
+import com.ezban.product.model.Product;
+import com.ezban.product.model.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+
+@Service
+public class ProductReportService {
+
+    @Autowired
+    private ProductReportRepository repository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    // 新增檢舉商品
+    public void addProductReport(@Valid AddProductReportDTO addProductReportDTO) {
+
+        ProductReport productReport = new ProductReport();
+        Product product = productRepository.findById(addProductReportDTO.getProductNo()).get();
+        productReport.setProduct(product);
+        Member member = memberRepository.findById(addProductReportDTO.getMemberNo()).get() ;
+        productReport.setMember(member);
+        productReport.setReportReason(addProductReportDTO.getReportReason());
+
+        repository.save(productReport);
+    }
+
+
+    // 顯示全部被檢舉的商品
+    public List<ProductReport> getAll() {
+        return repository.findAll();
+    }
+
+
+    // 更新審核的狀態
+    public void updateProductReport(UpdateProductReportDTO updateProductReportDTO, String adminName, Integer adminNo) {
+
+        Optional<ProductReport> optionalProductReport = repository.findById(updateProductReportDTO.getProductReportNo());
+        if (optionalProductReport.isPresent()) {
+            ProductReport productReport = optionalProductReport.get();
+            productReport.setReportStatus(updateProductReportDTO.getSelectedValue());
+//            Admin admin = adminRepository.findById(updateProductReportDTO.getAdminNo()).get();
+//            Admin adminNa = adminRepository.findById(updateProductReportDTO.getAdminName()).get();
+//            productReport.setAdmin(admin);
+//            productReport.setAdmin(adminNa);
+            repository.save(productReport);
+        }
+    }
+
+}
