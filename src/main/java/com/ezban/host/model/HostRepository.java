37c9cbefcc32f3ed72b2b55bbe6983381062a09b
Fred <fred890312@gmail.com>
2024-05-21 23:09:03 +0800

    新增findHostNameByEventNo、findHostByEventNo

--- src/main/java/com/ezban/host/model/HostRepository.java
+++ src/main/java/com/ezban/host/model/HostRepository.java
@@ -2,6 +2,8 @@ package com.ezban.host.model;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HostRepository extends JpaRepository<Host, Integer> {
     Optional<Host> findByHostAccount(String hostAccount);  
@@ -12,4 +14,9 @@ public interface HostRepository extends JpaRepository<Host, Integer> {
 
     Optional<Host> findHostByHostNo(Integer hostNo);
 
+    @Query("SELECT h.hostName FROM Host h JOIN Event e ON h.hostNo = e.host.hostNo WHERE e.eventNo = :eventNo")
+    String findHostNameByEventNo(@Param("eventNo") Integer eventNo);
+
+    @Query("SELECT h FROM Host h JOIN Event e ON h.hostNo = e.host.hostNo WHERE e.eventNo = :eventNo")
+    Optional<Host> findHostByEventNo(@Param("eventNo") Integer eventNo);
 }
