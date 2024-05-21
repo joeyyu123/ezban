09b2e53782b9dbd14966d2543d3cfe5595f12750
joey <joeyyu159357789@gmail.com>
2024-05-21 14:18:21 +0800

    在test中新增上傳圖片到資料庫的程式碼

---
+++ src/test/java/PhotoUploader.java
@@ -0,0 +1,44 @@
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class PhotoUploader {
+    public static void main(String[] args) throws IOException {
+        String url = "jdbc:mysql://localhost:3306/ezban";
+        String user = "root";
+        String password = "1qaz2wsx";
+
+        String filePath = "";
+
+
+        File folder = ResourceUtils.getFile(filePath);
+        File[] files = folder.listFiles();
+        for (File file : files) {
+
+            if (file.isFile() && file.getName().endsWith(".jpg")) {
+                // 讀取圖片檔案
+                int eventNo = Integer.parseInt(file.getName().split("\\.")[0]);
+
+                byte[] imageData = Files.readAllBytes(file.toPath());
+                try (Connection conn = DriverManager.getConnection(url, user, password)) {
+                    String sql = "UPDATE event SET event_img = ? WHERE event_no = ?";
+                    try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                        pstmt.setBytes(1, imageData);
+                        pstmt.setInt(2, eventNo);
+                        pstmt.executeUpdate();
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+}
