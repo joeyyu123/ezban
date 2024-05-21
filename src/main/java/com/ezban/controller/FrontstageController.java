eb34a322b7fad21a2d32d4f1db849e3a6d73430b
joey <joeyyu159357789@gmail.com>
2024-05-21 12:06:21 +0800

    移除FrontStageController中沒用到的依賴

--- src/main/java/com/ezban/controller/FrontstageController.java
+++ src/main/java/com/ezban/controller/FrontstageController.java
@@ -1,6 +1,5 @@
 package com.ezban.controller;
 
-import com.ezban.event.EventController;
 import com.ezban.event.model.Event;
 import com.ezban.event.model.EventDto;
 import com.ezban.event.model.Service.EventService;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +15,6 @@ import java.util.List;
 public class FrontstageController {
     @Autowired
     EventService eventService;
-
-    @Autowired
-    EventController eventController;
 
     @GetMapping("")
     public String index(Model model) {
