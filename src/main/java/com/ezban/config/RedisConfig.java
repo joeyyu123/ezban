e9eab184bb06e27e03c823d6ee20b0338684094f
erinchc <erinchen25@gmail.com>
2024-05-21 15:23:18 +0800

    合併兩個RedisConfig

--- src/main/java/com/ezban/config/RedisConfig.java
+++ src/main/java/com/ezban/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.ezban.event.model.EventDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -18,5 +19,10 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForHash();
     }
 }
