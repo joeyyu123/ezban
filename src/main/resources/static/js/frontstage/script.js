88aa3d203cc6dac5be7aa9314de6b1114ca09845
erinchc <erinchen25@gmail.com>
2024-05-22 23:37:27 +0800

    新增首頁商品，修改相關商品功能

--- src/main/resources/static/js/frontstage/script.js
+++ src/main/resources/static/js/frontstage/script.js
@@ -6,128 +6,112 @@
  */
 
 (function ($) {
-  'use strict';
+    'use strict';
 
-  // Preloader
-  $(window).on('load', function () {
-    $('#preloader').fadeOut('slow', function () {
-      $(this).remove();
+    // Preloader
+    $(window).on('load', function () {
+        $('#preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
     });
-  });
 
-  
-  // Instagram Feed
-  if (($('#instafeed').length) !== 0) {
-    var accessToken = $('#instafeed').attr('data-accessToken');
-    var userFeed = new Instafeed({
-      get: 'user',
-      resolution: 'low_resolution',
-      accessToken: accessToken,
-      template: '<a href="{{link}}"><img src="{{image}}" alt="instagram-image"></a>'
+
+    // Instagram Feed
+    if (($('#instafeed').length) !== 0) {
+        var accessToken = $('#instafeed').attr('data-accessToken');
+        var userFeed = new Instafeed({
+            get: 'user',
+            resolution: 'low_resolution',
+            accessToken: accessToken,
+            template: '<a href="{{link}}"><img src="{{image}}" alt="instagram-image"></a>'
+        });
+        userFeed.run();
+    }
+
+    setTimeout(function () {
+        $('.instagram-slider').slick({
+            dots: false,
+            speed: 300,
+            // autoplay: true,
+            arrows: false,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                }
+            ]
+        });
+    }, 1500);
+
+
+    // e-commerce touchspin
+    // $('input[name=\'product-quantity\']').TouchSpin();
+    $(document).ready(function () {
+        $('input[name="product-quantity"]').TouchSpin();
     });
-    userFeed.run();
-  }
 
-  setTimeout(function () {
-    $('.instagram-slider').slick({
-      dots: false,
-      speed: 300,
-      // autoplay: true,
-      arrows: false,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      responsive: [{
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2
-          }
-        }
-      ]
+
+    // Video Lightbox
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
     });
-  }, 1500);
 
 
-  // e-commerce touchspin
-  // $('input[name=\'product-quantity\']').TouchSpin();
-  $(document).ready(function() {
-    $('input[name="product-quantity"]').TouchSpin();
-  });
+    // Count Down JS
+    $('#simple-timer').syotimer({
+        year: 2022,
+        month: 5,
+        day: 9,
+        hour: 20,
+        minute: 30
+    });
 
-
-  // Video Lightbox
-  $(document).on('click', '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-  });
-
-
-  // Count Down JS
-  $('#simple-timer').syotimer({
-    year: 2022,
-    month: 5,
-    day: 9,
-    hour: 20,
-    minute: 30
-  });
-
-  //Hero Slider
-  $('.hero-slider').slick({
-    // autoplay: true,
-    infinite: true,
-    arrows: true,
-    prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
-    nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
-    dots: true,
-    autoplaySpeed: 7000,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
-  $('.hero-slider').slickAnimation();
-
-
-})(jQuery);
+    //Hero Slider
+//   $('.hero-slider').slick({
+//     // autoplay: true,
+//     infinite: true,
+//     arrows: true,
+//     prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
+//     nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
+//     dots: true,
+//     autoplaySpeed: 7000,
+//     pauseOnFocus: false,
+//     pauseOnHover: false
+//   });
+//   $('.hero-slider').slickAnimation();
+//
+//
+// })(jQuery);
 
 
 // carousel
-$(function(){
-
-    $(".autoplay").slick({
-        arrows: true,
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
+    $(document).ready(function () {
+        $('.autoplay').slick({
+            arrows: true,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+        });
     });
-});
+})(jQuery);
 
-// cart badge number
-// function getCartQuantity() {
-//   let url = `/cart/getCartQuantity`;
-//   fetch(url, {
-//     method: 'GET'
-//   })
-//       .then(response => response.json())
-//       .then(data => {
-//         if (data.cartQuantity != null) {
-//           document.querySelector('.cart-icon .badge').innerText = data.cartQuantity > 99 ? '99+' : data.cartQuantity;
-//         } else {
-//           console.error("無法取得購物車數量");
-//         }
-//       })
-//       .catch(error => console.error('Error:', error));
-// }
-// document.addEventListener('DOMContentLoaded',  getCartQuantity);
+
 
