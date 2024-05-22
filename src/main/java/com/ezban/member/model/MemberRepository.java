6a1550debf03d501a9ab114158bf4a5ad305db51
maxkuo625 <55643879+maxkuo625@users.noreply.github.com>
2024-05-22 13:21:47 +0800

    新增會員資料管理功能

--- src/main/java/com/ezban/member/model/MemberRepository.java
+++ src/main/java/com/ezban/member/model/MemberRepository.java
@@ -4,35 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 	Optional<Member> findByMemberMail(String memberMail);
 
-	Optional<Member> findByMemberName(String memberName);
-
-	Optional<Member> findByMemberPhone(String memberPhone);
-
-	List<Member> findByMemberStatus(Byte memberStatus);
-
-	@Modifying
-	@Transactional
-	@Query("UPDATE Member m SET m.memberStatus = ?1 WHERE m.memberMail = ?2")
-	void updateMemberStatus(Byte memberStatus, String memberMail);
-
-	@Modifying
-	@Transactional
-	@Query("UPDATE Member m SET m.memberPoints = ?1 WHERE m.memberMail = ?2")
-	void updateMemberPoints(Integer memberPoints, String memberMail);
-
 	// 生日優惠券排程用
 	@Query("SELECT m FROM Member m WHERE FUNCTION('MONTH', m.birthday) = :month AND m.memberStatus = :status")
-    List<Member> findMembersByBirthdayMonthAndStatus(int month, byte status);
+	List<Member> findMembersByBirthdayMonthAndStatus(int month, byte status);
 
 }
