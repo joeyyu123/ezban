57df7948bb01d3ea22d1d2c7b03e193ac261584f
Fred <fred890312@gmail.com>
2024-05-24 13:40:23 +0800

    更新首頁的活動收藏愛心

---
+++ src/main/resources/static/js/frontstage/saveevent/saveevent.js
@@ -0,0 +1,76 @@
+function initFavorites() {
+    const memberNo = document.getElementById('body').getAttribute('data-member-no');
+    fetch(`/saveevent/findFavoritesByMember/${memberNo}`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(event => {
+                const icon = document.querySelector(`a[data-event-no="${event.eventNo}"] i`);
+                if (icon) {
+                    icon.classList.add('favorite');
+                    icon.style.color = 'red';
+                }
+            });
+        })
+        .catch(error => {
+            console.error('初始化收藏狀態時出錯:', error);
+        });
+}
+
+function toggleFavorite(element, e) {
+    e.preventDefault();
+
+    const icon = element.querySelector('i');
+    const isFavorite = icon.classList.contains('favorite');
+    const eventNoStr = element.getAttribute('data-event-no');
+    const eventNo = parseInt(eventNoStr, 10);
+
+    if (isNaN(eventNo)) {
+        console.error('活動編號轉換失敗，值為:', eventNoStr);
+        return;
+    }
+
+    const newStatus = isFavorite ? 0 : 1;
+    const memberNo = document.getElementById('body').getAttribute('data-member-no');
+
+    const data = {
+        eventNo: eventNo,
+        memberNo: parseInt(memberNo, 10)
+    };
+
+    fetch('/saveevent/insert', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams(data).toString()
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                if (newStatus === 1) {
+                    icon.classList.add('favorite');
+                    icon.style.color = 'red';
+                    Swal.fire({
+                        title: '已收藏',
+                        text: '你已收藏該活動。',
+                        icon: 'success',
+                        confirmButtonText: '確定'
+                    });
+                } else {
+                    icon.classList.remove('favorite');
+                    icon.style.color = '';
+                    Swal.fire({
+                        title: '取消收藏',
+                        text: '你已取消收藏該活動。',
+                        icon: 'warning',
+                        confirmButtonText: '確定'
+                    });
+                }
+            } else {
+                console.error("收藏操作失敗");
+            }
+        })
+        .catch(error => {
+            console.error('錯誤:', error);
+        });
+}
