42c9ebc15e5ce603794f80653505de6278483dc1
joey <joeyyu159357789@gmail.com>
2024-05-26 11:06:38 +0800

    解決打包成jar檔後，找不到payment_conf.xm的問題

--- src/main/java/ecpay/payment/integration/AllInOneBase.java
+++ src/main/java/ecpay/payment/integration/AllInOneBase.java
@@ -7,8 +7,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
+import java.io.*;
 
 public class AllInOneBase {
 	protected static String operatingMode;
@@ -29,49 +28,62 @@ public class AllInOneBase {
 	protected static Document verifyDoc;
 	protected static String[] ignorePayment;
 	public AllInOneBase(){
-		try{
-			Document doc;
-			/* when using web project*/
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			String configPath = URLDecoder.decode(classLoader.getResource("payment_conf.xml").getPath(), "UTF-8");
-			doc = EcpayFunction.xmlParser(configPath);
-			/* when using testing code*/
+        Document doc;
+        /* when using web project*/
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("payment_conf.xml")) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource 'payment_conf.xml' not found");
+            }
+            // Create a temporary file to hold the XML content
+            File tempFile = File.createTempFile("payment_conf", ".xml");
+            tempFile.deleteOnExit();
+            try (FileOutputStream out = new FileOutputStream(tempFile)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            }
+            // Parse the temporary file
+            doc = EcpayFunction.xmlParser(tempFile.toURI().toString());
+        } catch (IOException e) {
+throw new RuntimeException(e);
+}
+        /* when using testing code*/
 //			String paymentConfPath = "./src/main/resources/payment_conf.xml";
 //			doc = EcpayFunction.xmlParser(paymentConfPath);
-			
-			doc.getDocumentElement().normalize();
-			//OperatingMode
-			Element ele = (Element)doc.getElementsByTagName("OperatingMode").item(0);
-			operatingMode = ele.getTextContent();
-			//MercProfile
-			ele = (Element)doc.getElementsByTagName("MercProfile").item(0);
-			mercProfile = ele.getTextContent();
-			//IsProjectContractor
-			ele = (Element)doc.getElementsByTagName("IsProjectContractor").item(0);
-			isProjectContractor = ele.getTextContent();
-			//MID, HashKey, HashIV, PlatformID
-			NodeList nodeList = doc.getElementsByTagName("MInfo");
-			for(int i = 0; i < nodeList.getLength(); i++){
-				ele = (Element)nodeList.item(i);
-				if(ele.getAttribute("name").equalsIgnoreCase(mercProfile)){
-					MerchantID = ele.getElementsByTagName("MerchantID").item(0).getTextContent();
-					HashKey = ele.getElementsByTagName("HashKey").item(0).getTextContent();
-					HashIV = ele.getElementsByTagName("HashIV").item(0).getTextContent();
-					PlatformID = isProjectContractor.equalsIgnoreCase("N")? "" : MerchantID;
-				}
-			}
-			// IgnorePayment
-			ele = (Element)doc.getElementsByTagName("IgnorePayment").item(0);
-			nodeList = ele.getElementsByTagName("Method");
-			ignorePayment = new String[nodeList.getLength()];
-			for(int i = 0; i < nodeList.getLength(); i++){
-				ignorePayment[i] = nodeList.item(i).getTextContent();
-			}
-			if(HashKey == null){
-				throw new EcpayException(ErrorMessage.MInfo_NOT_SETTING);
-			}
-		} catch(UnsupportedEncodingException e){
-			e.printStackTrace();
-		}
-	}
+
+        doc.getDocumentElement().normalize();
+        //OperatingMode
+        Element ele = (Element)doc.getElementsByTagName("OperatingMode").item(0);
+        operatingMode = ele.getTextContent();
+        //MercProfile
+        ele = (Element)doc.getElementsByTagName("MercProfile").item(0);
+        mercProfile = ele.getTextContent();
+        //IsProjectContractor
+        ele = (Element)doc.getElementsByTagName("IsProjectContractor").item(0);
+        isProjectContractor = ele.getTextContent();
+        //MID, HashKey, HashIV, PlatformID
+        NodeList nodeList = doc.getElementsByTagName("MInfo");
+        for(int i = 0; i < nodeList.getLength(); i++){
+            ele = (Element)nodeList.item(i);
+            if(ele.getAttribute("name").equalsIgnoreCase(mercProfile)){
+                MerchantID = ele.getElementsByTagName("MerchantID").item(0).getTextContent();
+                HashKey = ele.getElementsByTagName("HashKey").item(0).getTextContent();
+                HashIV = ele.getElementsByTagName("HashIV").item(0).getTextContent();
+                PlatformID = isProjectContractor.equalsIgnoreCase("N")? "" : MerchantID;
+            }
+        }
+        // IgnorePayment
+        ele = (Element)doc.getElementsByTagName("IgnorePayment").item(0);
+        nodeList = ele.getElementsByTagName("Method");
+        ignorePayment = new String[nodeList.getLength()];
+        for(int i = 0; i < nodeList.getLength(); i++){
+            ignorePayment[i] = nodeList.item(i).getTextContent();
+        }
+        if(HashKey == null){
+            throw new EcpayException(ErrorMessage.MInfo_NOT_SETTING);
+        }
+    }
 }
